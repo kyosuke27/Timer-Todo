93658b3f3e1a4bb92909ba2a6b8d10375d39ab47
kyosuke <kn1359028527@gmail.com>
2025-05-14 03:16:46 +0900

    admobでbanner広告の実装

--- Timer-Todo/AppDelegate.swift
+++ Timer-Todo/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Onboard
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        MobileAds.shared.start(completionHandler:nil)
         // Override point for customization after application launch.
         return true
     }
--- Timer-Todo/Controller/TimerViewController.swift
+++ Timer-Todo/Controller/TimerViewController.swift
@@ -6,20 +6,38 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class TimerViewController: UIViewController {
     @IBOutlet weak var timerText: UILabel!
     @IBOutlet weak var timerDate: UILabel!
     var timer: Timer!
     var date: Date!
+    var bannerView: BannerView!
 
     override func viewDidLoad() {
+        let viewWidth = view.frame.inset(by: view.safeAreaInsets).width
+        //        bannerView.adUnitID =
+        //            Bundle.main.object(
+        //                forInfoDictionaryKey: "AdMobSettingViewBannerAdUnitId")
+        //            as? String
+        let adaptiveSize = currentOrientationAnchoredAdaptiveBanner(
+            width: viewWidth)
+        bannerView = BannerView(
+            adSize: adaptiveSize)
+        bannerView.adUnitID =
+            Bundle.main.object(
+                forInfoDictionaryKey: "AdMobSettingViewBannerAdUnitId")
+            as? String
+        bannerView.rootViewController = self
+        addBannerViewToView(bannerView)
+        
         let date = Date()
         timerDate.text = TimerUtil.convertDateToDisplayFormat(date: date)
         // ダークモードの判定
         if UITraitCollection.current.userInterfaceStyle == .dark {
             timerDate.textColor = .black
-        }else{
+        } else {
             timerDate.textColor = .white
         }
         // タイマーの初期化
@@ -31,6 +49,29 @@ class TimerViewController: UIViewController {
             selector: #selector(updateTimer),
             userInfo: nil,
             repeats: true)
+    }
+    
+    func addBannerViewToView(_ bannerView: BannerView) {
+      bannerView.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(bannerView)
+      // This example doesn't give width or height constraints, as the provided
+      // ad size gives the banner an intrinsic content size to size the view.
+      view.addConstraints(
+        [NSLayoutConstraint(item: bannerView,
+                            attribute: .bottom,
+                            relatedBy: .equal,
+                            toItem: view.safeAreaLayoutGuide,
+                            attribute: .bottom,
+                            multiplier: 1,
+                            constant: 0),
+        NSLayoutConstraint(item: bannerView,
+                            attribute: .centerX,
+                            relatedBy: .equal,
+                            toItem: view,
+                            attribute: .centerX,
+                            multiplier: 1,
+                            constant: 0)
+        ])
     }
 
     @objc func updateTimer() {
